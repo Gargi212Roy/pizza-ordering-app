847cd7c426f9616bc6e5224e7593e9dc7ed72d26
Gargi Roy <itsgargiroy@gmail.com>
2023-04-02 11:35:34 +0530

    quantity displayed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import { useSelector } from "react-redux";
 import ProductCard from "./ProductCard";
 import EmptyCartImage from "../assets/empty-cart3.webp";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import "./style.css";
 
 function Cart() {
@@ -16,10 +17,38 @@ function Cart() {
       <div className={cart.cart.length === 0 ? "image" : "no-image"}>
         <img src={EmptyCartImage} alt="Empty Card" className="img-size" />
       </div>
-      <div className="container">
-        {cart.cart.map((product) => (
-          <ProductCard product={product.product} key={product.id} />
-        ))}
+      <div className="flex-cart">
+        <div className="container">
+          {cart.cart.map((product) => (
+            <ProductCard product={product.product} key={product.id} />
+          ))}
+        </div>
+        <div className="container ">
+          <h1 className={cart.cart.length === 0 ? "no-image" : ""}>
+            {" "}
+            Quantity
+          </h1>
+
+          {cart.cart.map((product) => (
+            <Quantity product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
+
+      <div></div>
+    </>
+  );
+}
+
+function Quantity({ product }) {
+  return (
+    <>
+      <div className=" cart-quantity-style">
+        <p className="quantity-padding">Product name: {product.product.name}</p>
+        <p className="quantity-padding">Quantity: {product.quantity}</p>
+        <p className="quantity-padding">
+          Price: Rs {product.product.price} per piece
+        </p>
       </div>
     </>
   );
